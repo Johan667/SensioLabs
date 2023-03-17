0046b57eff2407c228c4c29f89e69726dcad5387
johan <johan.kasri@icloud.com>
2023-03-17 10:34:37 +0100

    commentform

--- src/Form/CommentFormType.php
+++ src/Form/CommentFormType.php
@@ -3,7 +3,14 @@
 namespace App\Form;
 
 use App\Entity\Comment;
+use App\Entity\Conference;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +19,18 @@ class CommentFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('author')
-            ->add('text')
-            ->add('email')
-            ->add('createdAt')
-            ->add('picture')
-            ->add('conference')
+    ->add('author', TextType::class, [
+    'label' => 'Auteur'
+    ])
+    ->add('text', TextareaType::class, [
+        'label' => 'Texte'
+    ])
+
+    ->add('picture', FileType::class, [
+        'label' => 'Image'
+    ])
+;
+
         ;
     }
 
